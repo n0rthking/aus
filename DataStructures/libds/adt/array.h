f8b718869a19c0855486ed3d523310d5fb410d76
David Nemec <nemec@stud.uniza.sk>
2023-04-04 21:32:01 +0200

    week 7 but not done

--- DataStructures/libds/adt/array.h
+++ DataStructures/libds/adt/array.h
@@ -5,361 +5,359 @@
 
 namespace ds::adt {
 
-    class Dimension
-    {
-    public:
-        Dimension();
-        Dimension(int base, size_t size);
-        Dimension(const Dimension& other);
+	class Dimension
+	{
+	public:
+		Dimension(long long base, size_t size);
 
-        int getBase() const;
-        size_t getSize() const;
+		long long getBase() const;
+		size_t getSize() const;
 
-        bool operator==(const Dimension& other) const;
-        bool operator!=(const Dimension& other) const;
-        Dimension& operator=(const Dimension& other);
+		bool operator==(const Dimension& other) const;
+		bool operator!=(const Dimension& other) const;
 
-    private:
-        int base_;
-        size_t size_;
-    };
+	private:
+		long long base_;
+		size_t size_;
+	};
 
-    //----------
+	//----------
 
-    template <typename T>
-    class Array :
-        public ADS<T>
-    {
-    public:
-        using IteratorType = typename amt::IS<T>::IteratorType;
+	template <typename T>
+	class Array :
+		public ADS<T>
+	{
+	public:
+		using IteratorType = typename amt::IS<T>::IteratorType;
 
-    public:
-        Array(size_t size);
-        Array(Dimension dimension);
-        Array(const Array<T>& other);
-        ~Array();
+	public:
+		Array(size_t size);
+		Array(Dimension dimension);
+		Array(const Array<T>& other);
 
-        ADT& assign(const ADT& other) override;
-        void clear() override;
-        bool isEmpty() const override;
-        bool equals(const ADT& other) override;
+		ADT& assign(const ADT& other) override;
+		void clear() override;
+		bool equals(const ADT& other) override;
+		bool isEmpty() const override;
+		size_t size() const override;
+		long long getBase() const;
 
-        size_t size() const override;
-        int getBase() const;
-        T access(size_t index) const;
-        void set(T element, size_t index);
+	    T access(long long index) const;
+		void set(T element, long long index);
 
-        IteratorType begin();
-        IteratorType end();
+		IteratorType begin();
+		IteratorType end();
 
-    private:
-        bool validateIndex(size_t index) const;
-        size_t mapIndex(size_t index) const;
-        amt::IS<T>* getSequence() const;
+	private:
+		bool validateIndex(long long index) const;
+		size_t mapIndex(long long index) const;
+		amt::IS<T>* getSequence() const;
 
-    private:
-        int base_;
-    };
+	private:
+		long long base_;
+	};
 
-    //----------
+	//----------
 
-    template <typename T>
-    class Matrix :
-        public ADS<T>
-    {
-    public:
-        Matrix(size_t size1, size_t size2);
-        Matrix(Dimension dimension1, Dimension dimension2);
-        Matrix(const Matrix& other);
-        ~Matrix();
+	template <typename T>
+	class Matrix :
+		virtual public ADT
+	{
+	public:
+		virtual T access(long long index1, long long index2) const = 0;
+		virtual void set(T element, long long index1, long long index2) = 0;
+	};
 
-        ADT& assign(const ADT& other) override;
-        void clear() override;
-        size_t size() const override;
-        bool isEmpty() const override;
-        bool equals(const ADT& other) override;
+	//----------
 
-        T access(size_t index1, size_t index2) const;
-        void set(T element, size_t index1, size_t index2);
+	template <typename T>
+	class CompactMatrix :
+	    public Matrix<T>,
+		public ADS<T>
+	{
+	public:
+		CompactMatrix(size_t size1, size_t size2);
+		CompactMatrix(Dimension dimension1, Dimension dimension2);
+		CompactMatrix(const CompactMatrix& other);
 
-    private:
-        bool validateIndices(size_t index1, size_t index2) const;
-        size_t mapIndices(size_t index1, size_t index2) const;
-        amt::IS<T>* getSequence() const;
+		ADT& assign(const ADT& other) override;
+		void clear() override;
+		bool equals(const ADT& other) override;
+		bool isEmpty() const override;
+		size_t size() const override;
+		Dimension getDimension1() const;
+		Dimension getDimension2() const;
 
-    private:
-        Dimension dimension1_;
-        Dimension dimension2_;
-    };
+	    T access(long long index1, long long index2) const override;
+		void set(T element, long long index1, long long index2) override;
 
-    //----------
+	private:
+		bool validateIndices(long long index1, long long index2) const;
+		size_t mapIndices(long long index1, long long index2) const;
+		amt::IS<T>* getSequence() const;
 
-    inline Dimension::Dimension() :
-        Dimension(0, 0)
-    {
-    }
+	private:
+		Dimension dimension1_;
+		Dimension dimension2_;
+	};
 
-    inline Dimension::Dimension(int base, size_t size) :
-        base_(base),
-        size_(size)
-    {
-    }
+	//----------
 
-    inline Dimension::Dimension(const Dimension& other) :
-        base_(other.base_),
-        size_(other.size_)
-    {
-    }
+	inline Dimension::Dimension(long long base, size_t size) :
+		base_(base),
+		size_(size)
+	{
+	}
 
-    inline int Dimension::getBase() const
-    {
-        return base_;
-    }
+	inline long long Dimension::getBase() const
+	{
+		return base_;
+	}
 
-    inline size_t Dimension::getSize() const
-    {
-        return size_;
-    }
+	inline size_t Dimension::getSize() const
+	{
+		return size_;
+	}
 
-    inline bool Dimension::operator==(const Dimension& other) const
-    {
-        return base_ == other.base_ && size_ == other.size_;
-    }
+	inline bool Dimension::operator==(const Dimension& other) const
+	{
+		return base_ == other.base_ && size_ == other.size_;
+	}
 
     inline bool Dimension::operator!=(const Dimension& other) const
     {
-        return !(*this == other);
+		return !(*this == other);
     }
 
-    inline Dimension& Dimension::operator=(const Dimension& other)
-    {
-        base_ = other.base_;
-        size_ = other.size_;
-        return *this;
-    }
+	//----------
 
-    //----------
+	template<typename T>
+	Array<T>::Array(size_t size) :
+		Array(Dimension(0, size))
+	{
+	}
 
-    template<typename T>
-    Array<T>::Array(size_t size) :
-        Array(Dimension(0, size))
-    {
-    }
+	template<typename T>
+	Array<T>::Array(Dimension dimension) :
+		ADS<T>(new amt::IS<T>(dimension.getSize(), true)),
+		base_(dimension.getBase())
+	{
+	}
 
-    template<typename T>
-    Array<T>::Array(Dimension dimension) :
-        ADS<T>(new amt::IS<T>(dimension.getSize(), true)),
-        base_(dimension.getBase())
-    {
-    }
+	template<typename T>
+	Array<T>::Array(const Array<T>& other) :
+		ADS<T>(new amt::IS<T>(), other),
+		base_(other.base_)
+	{
+	}
 
-    template<typename T>
-    Array<T>::Array(const Array<T>& other) :
-        ADS<T>(new amt::IS<T>(), other),
-        base_(other.base_)
-    {
-    }
-
-    template<typename T>
-    Array<T>::~Array()
-    {
-    }
-
-    template<typename T>
-    ADT& Array<T>::assign(const ADT& other)
-    {
-        // TODO 07
+	template<typename T>
+	ADT& Array<T>::assign(const ADT& other)
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T>
-    void Array<T>::clear()
-    {
-        this->error("Array can't be cleared!");
-    }
+	template<typename T>
+	void Array<T>::clear()
+	{
+		this->error("Array can't be cleared!");
+	}
 
-    template<typename T>
-    size_t Array<T>::size() const
-    {
-        return this->getSequence()->size();
-    }
+	template<typename T>
+	bool Array<T>::equals(const ADT& other)
+	{
+		const Array<T>* otherArray = dynamic_cast<const Array<T>*>(&other);
+		return otherArray != nullptr &&
+			base_ == otherArray->base_ &&
+			ADS<T>::equals(other);
+	}
 
-    template<typename T>
-    int Array<T>::getBase() const
-    {
-        return base_;
-    }
-
-    template<typename T>
-    bool Array<T>::isEmpty() const
-    {
-        // TODO 07
+	template<typename T>
+	bool Array<T>::isEmpty() const
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T>
-    bool Array<T>::equals(const ADT& other)
-    {
-        const Array<T>& otherArray = dynamic_cast<const Array<T>&>(other);
-        return base_ == otherArray.base_ && ADS<T>::equals(other);
-    }
+	template<typename T>
+	size_t Array<T>::size() const
+	{
+		return this->getSequence()->size();
+	}
 
-    template<typename T>
-    T Array<T>::access(size_t index) const
-    {
-        // TODO 07
+	template<typename T>
+	long long Array<T>::getBase() const
+	{
+		return base_;
+	}
+
+	template<typename T>
+	T Array<T>::access(long long index) const
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T>
-    void Array<T>::set(T element, size_t index)
-    {
-        // TODO 07
+	template<typename T>
+	void Array<T>::set(T element, long long index)
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
+	}
+
+	template <typename T>
+	auto Array<T>::begin() -> IteratorType
+	{
+		// TODO 07
+        // po implementacii vymazte vyhodenie vynimky!
+        throw std::runtime_error("Not implemented yet");
+	}
+
+	template <typename T>
+	auto Array<T>::end() -> IteratorType
+	{
+		// TODO 07
+        // po implementacii vymazte vyhodenie vynimky!
+        throw std::runtime_error("Not implemented yet");
+	}
+
+	template<typename T>
+	amt::IS<T>* Array<T>::getSequence() const
+	{
+		return dynamic_cast<amt::IS<T>*>(this->memoryStructure_);
+	}
+
+	template<typename T>
+	bool Array<T>::validateIndex(long long index) const
+	{
+		// TODO 07
+        // po implementacii vymazte vyhodenie vynimky!
+        throw std::runtime_error("Not implemented yet");
+	}
+
+	template<typename T>
+	size_t Array<T>::mapIndex(long long index) const
+	{
+		// TODO 07
+        // po implementacii vymazte vyhodenie vynimky!
+        throw std::runtime_error("Not implemented yet");
+	}
+
+	//----------
+
+	template<typename T>
+	CompactMatrix<T>::CompactMatrix(size_t size1, size_t size2) :
+		CompactMatrix<T>(Dimension(0, size1), Dimension(0, size2))
+	{
+	}
+
+	template<typename T>
+	CompactMatrix<T>::CompactMatrix(Dimension dimension1, Dimension dimension2) :
+		ADS<T>(new amt::IS<T>(dimension1.getSize() * dimension2.getSize(), true)),
+	    dimension1_(dimension1),
+	    dimension2_(dimension2)
+	{
+	}
+
+	template<typename T>
+	CompactMatrix<T>::CompactMatrix(const CompactMatrix<T>& other) :
+		ADS<T>(new amt::IS<T>(), other),
+	    dimension1_(other.dimension1_),
+	    dimension2_(other.dimension2_)
+	{
+	}
+
+	template<typename T>
+	ADT& CompactMatrix<T>::assign(const ADT& other)
+	{
+		const CompactMatrix<T>& otherMatrix = dynamic_cast<const CompactMatrix<T>&>(other);
+		if (dimension1_ != otherMatrix.dimension1_ || dimension2_ != otherMatrix.dimension2_)
+		{
+			this->error("CompactMatrix dimensions are different!");
+		}
+		ADS<T>::assign(otherMatrix);
+		return *this;
+	}
+
+	template<typename T>
+	void CompactMatrix<T>::clear()
+	{
+		this->error("CompactMatrix can't be cleared!");
+	}
+
+	template<typename T>
+	bool CompactMatrix<T>::equals(const ADT& other)
+	{
+		const CompactMatrix<T>* otherMatrix = dynamic_cast<const CompactMatrix<T>*>(&other);
+		return otherMatrix != nullptr &&
+			dimension1_ == otherMatrix->dimension1_ &&
+			dimension2_ == otherMatrix->dimension2_ &&
+			ADS<T>::equals(other);
+	}
+
+	template<typename T>
+	bool CompactMatrix<T>::isEmpty() const
+	{
+		return false;
+	}
+
+	template<typename T>
+	size_t CompactMatrix<T>::size() const
+	{
+		return dimension1_.getSize() * dimension2_.getSize();
+	}
+
+    template <typename T>
+    Dimension CompactMatrix<T>::getDimension1() const
+    {
+		return dimension1_;
     }
 
     template <typename T>
-    auto Array<T>::begin() -> IteratorType
+    Dimension CompactMatrix<T>::getDimension2() const
     {
-        // TODO 07
+		return dimension2_;
+    }
+
+	template<typename T>
+	T CompactMatrix<T>::access(long long index1, long long index2) const
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
-
-    template <typename T>
-    auto Array<T>::end() -> IteratorType
-    {
-        // TODO 07
+	}
+	template<typename T>
+	void CompactMatrix<T>::set(T element, long long index1, long long index2)
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T>
-    amt::IS<T>* Array<T>::getSequence() const
-    {
-        return static_cast<amt::IS<T>*>(this->memoryStructure_);
-    }
-
-    template<typename T>
-    bool Array<T>::validateIndex(size_t index) const
-    {
-        // TODO 07
+	template<typename T>
+	bool CompactMatrix<T>::validateIndices(long long index1, long long index2) const
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T>
-    size_t Array<T>::mapIndex(size_t index) const
-    {
-        // TODO 07
+	template<typename T>
+	size_t CompactMatrix<T>::mapIndices(long long index1, long long index2) const
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    //----------
-
-    template<typename T>
-    Matrix<T>::Matrix(size_t size1, size_t size2) :
-        Matrix<T>(Dimension(0, size1), Dimension(0, size2))
-    {
-    }
-
-    template<typename T>
-    Matrix<T>::Matrix(Dimension dimension1, Dimension dimension2) :
-        ADS<T>(new amt::IS<T>(dimension1.getSize() * dimension2.getSize(), true)),
-        dimension1_(dimension1),
-        dimension2_(dimension2)
-    {
-    }
-
-    template<typename T>
-    Matrix<T>::Matrix(const Matrix<T>& other) :
-        ADS<T>(new amt::IS<T>(), other)
-    {
-    }
-
-    template<typename T>
-    Matrix<T>::~Matrix()
-    {
-    }
-
-    template<typename T>
-    ADT& Matrix<T>::assign(const ADT& other)
-    {
-        const Matrix<T>& otherMatrix = dynamic_cast<const Matrix<T>&>(other);
-        if (dimension1_ != otherMatrix.dimension1_ || dimension2_ != otherMatrix.dimension2_)
-        {
-            this->error("Matrix dimensions are different!");
-        }
-        ADS<T>::assign(otherMatrix);
-        return *this;
-    }
-
-    template<typename T>
-    void Matrix<T>::clear()
-    {
-        this->error("Matrix can't be cleared!");
-    }
-
-    template<typename T>
-    size_t Matrix<T>::size() const
-    {
-        return dimension1_.getSize() * dimension2_.getSize();
-    }
-
-    template<typename T>
-    bool Matrix<T>::isEmpty() const
-    {
-        return false;
-    }
-
-    template<typename T>
-    bool Matrix<T>::equals(const ADT& other)
-    {
-        const Matrix<T>& otherMatrix = dynamic_cast<const Matrix<T>&>(other);
-        return dimension1_ == otherMatrix.dimension1_ &&
-               dimension2_ == otherMatrix.dimension2_ &&
-               ADS<T>::equals(other);
-    }
-
-    template<typename T>
-    T Matrix<T>::access(size_t index1, size_t index2) const
-    {
-        // TODO 07
-        // po implementacii vymazte vyhodenie vynimky!
-        throw std::runtime_error("Not implemented yet");
-    }
-    template<typename T>
-    void Matrix<T>::set(T element, size_t index1, size_t index2)
-    {
-        // TODO 07
-        // po implementacii vymazte vyhodenie vynimky!
-        throw std::runtime_error("Not implemented yet");
-    }
-
-    template<typename T>
-    bool Matrix<T>::validateIndices(size_t index1, size_t index2) const
-    {
-        // TODO 07
-        // po implementacii vymazte vyhodenie vynimky!
-        throw std::runtime_error("Not implemented yet");
-    }
-
-    template<typename T>
-    size_t Matrix<T>::mapIndices(size_t index1, size_t index2) const
-    {
-        // TODO 07
-        // po implementacii vymazte vyhodenie vynimky!
-        throw std::runtime_error("Not implemented yet");
-    }
-
-    template<typename T>
-    amt::IS<T>* Matrix<T>::getSequence() const
-    {
-        return static_cast<amt::IS<T>*>(this->memoryStructure_);
-    }
+	template<typename T>
+	amt::IS<T>* CompactMatrix<T>::getSequence() const
+	{
+		return dynamic_cast<amt::IS<T>*>(this->memoryStructure_);
+	}
 }
--- DataStructures/libds/adt/list.h
+++ DataStructures/libds/adt/list.h
@@ -6,244 +6,244 @@
 
 namespace ds::adt {
 
-    template <typename T>
-    class List :
-        virtual public ADT
-    {
-    public:
-        virtual size_t calculateIndex(T element) = 0;
+	template <typename T>
+	class List :
+		virtual public ADT
+	{
+	public:
+		virtual size_t calculateIndex(T element) = 0;
 
-        virtual bool contains(T element) = 0;
+		virtual bool contains(T element) = 0;
 
-        virtual T accessFirst() = 0;
-        virtual T accessLast() = 0;
-        virtual T access(size_t index) = 0;
+		virtual T accessFirst() = 0;
+		virtual T accessLast() = 0;
+		virtual T access(size_t index) = 0;
 
-        virtual void insertFirst(T element) = 0;
-        virtual void insertLast(T element) = 0;
-        virtual void insert(T element, size_t index) = 0;
+		virtual void insertFirst(T element) = 0;
+		virtual void insertLast(T element) = 0;
+		virtual void insert(T element, size_t index) = 0;
 
-        virtual void set(size_t index, T element) = 0;
+		virtual void set(size_t index, T element) = 0;
 
-        virtual void removeFirst() = 0;
-        virtual void removeLast() = 0;
-        virtual void remove(size_t index) = 0;
-    };
+		virtual void removeFirst() = 0;
+		virtual void removeLast() = 0;
+		virtual void remove(size_t index) = 0;
+	};
 
-    //----------
+	//----------
 
-    template <typename T, typename SequenceType>
-    class GeneralList :
-        virtual public List<T>,
-        public ADS<T>
-    {
-    public:
-        using IteratorType = typename SequenceType::IteratorType;
+	template <typename T, typename SequenceType>
+	class GeneralList :
+		public List<T>,
+		public ADS<T>
+	{
+	public:
+		using IteratorType = typename SequenceType::IteratorType;
 
-    public:
-        GeneralList();
-        GeneralList(const GeneralList& other);
+	public:
+		GeneralList();
+		GeneralList(const GeneralList& other);
 
-        size_t calculateIndex(T element) override;
+		size_t calculateIndex(T element) override;
 
-        bool contains(T element) override;
+		bool contains(T element) override;
 
-        T accessFirst() override;
-        T accessLast() override;
-        T access(size_t index) override;
+		T accessFirst() override;
+		T accessLast() override;
+		T access(size_t index) override;
 
-        void insertFirst(T element) override;
-        void insertLast(T element) override;
-        void insert(T element, size_t index) override;
+		void insertFirst(T element) override;
+		void insertLast(T element) override;
+		void insert(T element, size_t index) override;
 
-        void set(size_t index, T element) override;
+		void set(size_t index, T element) override;
 
-        void removeFirst() override;
-        void removeLast() override;
-        void remove(size_t index) override;
+		void removeFirst() override;
+		void removeLast() override;
+		void remove(size_t index) override;
 
-        IteratorType begin();
-        IteratorType end();
+		IteratorType begin();
+		IteratorType end();
 
-    protected:
-        SequenceType* getSequence() const;
-    };
+	protected:
+		SequenceType* getSequence() const;
+	};
 
-    //----------
+	//----------
 
-    template <typename T>
-    class ImplicitList :
-        public GeneralList<T, amt::IS<T>>
-    {
-    };
+	template <typename T>
+	class ImplicitList :
+		public GeneralList<T, amt::IS<T>>
+	{
+	};
 
-    //----------
+	//----------
 
-    template <typename T>
-    class ImplicitCyclicList :
-        public GeneralList<T, amt::CIS<T>>
-    {
-    };
+	template <typename T>
+	class ImplicitCyclicList :
+		public GeneralList<T, amt::CIS<T>>
+	{
+	};
 
-    //----------
+	//----------
 
-    template <typename T>
-    class SinglyLinkedList :
-        public GeneralList<T, amt::SinglyLS<T>>
-    {
-    };
+	template <typename T>
+	class SinglyLinkedList :
+		public GeneralList<T, amt::SinglyLS<T>>
+	{
+	};
 
-    //----------
+	//----------
 
-    template <typename T>
-    class SinglyCyclicLinkedList :
-        public GeneralList<T, amt::SinglyCLS<T>>
-    {
-    };
+	template <typename T>
+	class SinglyCyclicLinkedList :
+		public GeneralList<T, amt::SinglyCLS<T>>
+	{
+	};
 
-    //----------
+	//----------
 
-    template <typename T>
-    class DoublyLinkedList :
-        public GeneralList<T, amt::DoublyLS<T>>
-    {
-    };
+	template <typename T>
+	class DoublyLinkedList :
+		public GeneralList<T, amt::DoublyLS<T>>
+	{
+	};
 
-    //----------
+	//----------
 
-    template <typename T>
-    class DoublyCyclicLinkedList :
-        public GeneralList<T, amt::DoublyCLS<T>>
-    {
-    };
+	template <typename T>
+	class DoublyCyclicLinkedList :
+		public GeneralList<T, amt::DoublyCLS<T>>
+	{
+	};
 
-    //----------
+	//----------
 
-    template<typename T, typename SequenceType>
-    GeneralList<T, SequenceType>::GeneralList() :
-        ADS<T>(new SequenceType())
-    {
-    }
+	template<typename T, typename SequenceType>
+	GeneralList<T, SequenceType>::GeneralList() :
+		ADS<T>(new SequenceType())
+	{
+	}
 
-    template<typename T, typename SequenceType>
-    GeneralList<T, SequenceType>::GeneralList(const GeneralList& other) :
-        ADS<T>(new SequenceType(), other)
-    {
-    }
+	template<typename T, typename SequenceType>
+	GeneralList<T, SequenceType>::GeneralList(const GeneralList& other) :
+		ADS<T>(new SequenceType(), other)
+	{
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     size_t GeneralList<T, SequenceType>::calculateIndex(T element)
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     bool GeneralList<T, SequenceType>::contains(T element)
-    {
-        return this->calculateIndex(element) != INVALID_INDEX;
-    }
+	{
+		return this->calculateIndex(element) != INVALID_INDEX;
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     T GeneralList<T, SequenceType>::accessFirst()
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     T GeneralList<T, SequenceType>::accessLast()
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     T GeneralList<T, SequenceType>::access(size_t index)
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     void GeneralList<T, SequenceType>::insertFirst(T element)
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     void GeneralList<T, SequenceType>::insertLast(T element)
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     void GeneralList<T, SequenceType>::insert(T element, size_t index)
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     void GeneralList<T, SequenceType>::set(size_t index, T element)
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     void GeneralList<T, SequenceType>::removeFirst()
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     void GeneralList<T, SequenceType>::removeLast()
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     void GeneralList<T, SequenceType>::remove(size_t index)
-    {
-        // TODO 07
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template <typename T, typename SequenceType>
-    auto GeneralList<T, SequenceType>::begin() -> IteratorType
-    {
-        // TODO 07
+	template <typename T, typename SequenceType>
+	auto GeneralList<T, SequenceType>::begin() -> IteratorType
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template <typename T, typename SequenceType>
-    auto GeneralList<T, SequenceType>::end() -> IteratorType
-    {
-        // TODO 07
+	template <typename T, typename SequenceType>
+	auto GeneralList<T, SequenceType>::end() -> IteratorType
+	{
+		// TODO 07
         // po implementacii vymazte vyhodenie vynimky!
         throw std::runtime_error("Not implemented yet");
-    }
+	}
 
-    template<typename T, typename SequenceType>
+	template<typename T, typename SequenceType>
     SequenceType* GeneralList<T, SequenceType>::getSequence() const
-    {
-        return static_cast<SequenceType*>(this->memoryStructure_);
-    }
+	{
+		return dynamic_cast<SequenceType*>(this->memoryStructure_);
+	}
 }
