a55d8ae67e4a73b5426dcdd9c8037748eb27f169
David Nemec <nemec@stud.uniza.sk>
2023-04-04 13:28:42 +0200

    refactor to own methods

--- Semestralka/DruhaUroven.cpp
+++ Semestralka/DruhaUroven.cpp
@@ -2,16 +2,31 @@
 
 DruhaUroven::DruhaUroven()
 {
+    hierarchy.emplaceRoot().data_.officialTitle = "Slovensko";
+
+    VytvorHierarchiu();
+
+    for (auto it = hierarchy.beginPre(); it != hierarchy.endPre(); ++it) {
+        std::cout << (*it).toStr() << std::endl;
+    }
+}
+
+void DruhaUroven::VytvorHierarchiu()
+{
+    NacitajKraje();
+    NacitajOkresy();
+    NacitajObce();
+}
+
+void DruhaUroven::NacitajKraje()
+{
     Citac citacKraje("data/kraje.csv");
     citacKraje.preskocPrvyRiadok();
-
-    auto& sk = hierarchy.emplaceRoot();
-    sk.data_.officialTitle = "Slovensko";
 
     size_t counter = 0;
     while (citacKraje.citajRiadok()) {
         UzemnaJednotka uj = citacKraje.vytvorUJ();
-        auto& curSon = hierarchy.emplaceSon(sk, counter);
+        auto& curSon = hierarchy.emplaceSon(*hierarchy.accessRoot(), counter);
 
         curSon.data_.sortNumber = uj.sortNumber;
         curSon.data_.code = uj.code;
@@ -24,15 +39,18 @@ DruhaUroven::DruhaUroven()
     }
 
     citacKraje.zatvorSubor();
+}
 
+void DruhaUroven::NacitajOkresy()
+{
     Citac citacOkresy("data/okresy.csv");
     citacOkresy.preskocPrvyRiadok();
-    int zahrCounter = 0;
 
+    int zahrCounter = 0;
     while (citacOkresy.citajRiadok()) {
         UzemnaJednotka uj = citacOkresy.vytvorUJ();
         if (uj.note.length() <= 1) {
-            auto& currentKraj = *sk.sons_->access(8)->data_;
+            auto& currentKraj = *hierarchy.accessRoot()->sons_->access(8)->data_;
             auto& curSon = hierarchy.emplaceSon(currentKraj, zahrCounter);
 
             curSon.data_.sortNumber = uj.sortNumber;
@@ -47,7 +65,7 @@ DruhaUroven::DruhaUroven()
         }
         int krajIndex = std::stoi(uj.note.substr(0, 1)) - 1;
         int sonIndex = std::stoi(uj.note.substr(uj.note.length() - 2, 2)) - 1;
-        auto& currentKraj = *sk.sons_->access(krajIndex)->data_;
+        auto& currentKraj = *hierarchy.accessRoot()->sons_->access(krajIndex)->data_;
         auto& curSon = hierarchy.emplaceSon(currentKraj, sonIndex);
 
         curSon.data_.sortNumber = uj.sortNumber;
@@ -60,7 +78,10 @@ DruhaUroven::DruhaUroven()
     }
 
     citacOkresy.zatvorSubor();
+}
 
+void DruhaUroven::NacitajObce()
+{
     Citac citacObce("data/obce.csv");
     citacObce.preskocPrvyRiadok();
 
@@ -71,39 +92,39 @@ DruhaUroven::DruhaUroven()
 
         int krajIndex = 8;
         if (uj.code.find("ZZZ") != std::string::npos) {
-            std::cout << "BDSFJHKDJHGJKDFGJDHKFGJHDKJGF\n";
+            //std::cout << "BDSFJHKDJHGJKDFGJDHKFGJHDKJGF\n";
             continue;
         }
         int cleanCodeInt = std::stoi(cleanCode);
         switch (cleanCodeInt) {
-            case 10:
-                krajIndex = 0;
-                break;
-            case 21 :
-                krajIndex = 1;
-                break;
-            case 22:
-                krajIndex = 2;
-                break;
-            case 23:
-                krajIndex = 3;
-                break;
-            case 31:
-                krajIndex = 4;
-                break;
-            case 32:
-                krajIndex = 5;
-                break;
-            case 41:
-                krajIndex = 6;
-                break;
-            case 42:
-                krajIndex = 7;
-                break;
+        case 10:
+            krajIndex = 0;
+            break;
+        case 21:
+            krajIndex = 1;
+            break;
+        case 22:
+            krajIndex = 2;
+            break;
+        case 23:
+            krajIndex = 3;
+            break;
+        case 31:
+            krajIndex = 4;
+            break;
+        case 32:
+            krajIndex = 5;
+            break;
+        case 41:
+            krajIndex = 6;
+            break;
+        case 42:
+            krajIndex = 7;
+            break;
         }
 
         int okresIndex = std::stoi(uj.code.substr(5, 1), nullptr, 16) - 1;
-        auto& currentOkres = *sk.sons_->access(krajIndex)->data_->sons_->access(okresIndex)->data_;
+        auto& currentOkres = *hierarchy.accessRoot()->sons_->access(krajIndex)->data_->sons_->access(okresIndex)->data_;
         int fuckingIndex = currentOkres.sons_->size() - 1;
         auto& curSon = hierarchy.emplaceSon(currentOkres, fuckingIndex <= 0 ? 0 : fuckingIndex);
 
@@ -117,8 +138,4 @@ DruhaUroven::DruhaUroven()
     }
 
     citacObce.zatvorSubor();
-
-    for (auto it = hierarchy.beginPre(); it != hierarchy.endPre(); ++it) {
-        std::cout << (*it).toStr() << std::endl;
-    }
 }
--- Semestralka/DruhaUroven.h
+++ Semestralka/DruhaUroven.h
@@ -2,22 +2,23 @@
 
 #include <iostream>
 #include <string>
-#include <libds/amt/implicit_sequence.h>
 #include <libds/amt/explicit_hierarchy.h>
 #include "Algorithm.h"
 #include "UzemnaJednotka.h"
 #include "Citac.h"
 
 using DataType = UzemnaJednotka;
-using InputType = ds::amt::ImplicitSequence<DataType>;
 using ResultType = ds::amt::MultiWayExplicitHierarchy<DataType>;
 
 class DruhaUroven
 {
 private:
-	InputType inputSequence;
 	ResultType hierarchy;
 public:
 	DruhaUroven();
+	void VytvorHierarchiu();
+	void NacitajKraje();
+	void NacitajOkresy();
+	void NacitajObce();
 };
 
