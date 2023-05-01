47ee3f86844983bce7aa65ab071cbe15c5b6af69
David Nemec <nemec@stud.uniza.sk>
2023-05-01 23:31:39 +0200

    another fixes for first level

--- Semestralka/PrvaUroven.h
+++ Semestralka/PrvaUroven.h
@@ -12,12 +12,86 @@ private:
 	const int OPT_STARTSWITH = 2;
 	const int OPT_INVALID = -1;
 	SequenceType<UzemnaJednotka> outputSequence;
-	std::string whichFile;
-	std::string userInput;
+	std::string unitType;
+	std::string subString;
 public:
-	PrvaUroven(const DatovaUroven& other);
-	int zistiParametre();
-	void filtrujZaznamy(const std::string& subString, int retVal);
-	void vypisVystup();
+    PrvaUroven(const DatovaUroven& other) : DatovaUroven(other)
+    {
+        int retVal = zistiParametre();
+        if (retVal != OPT_INVALID) {
+            std::function<bool(UzemnaJednotka)> lambdaContains = [&](const auto& uj) { return uj.officialTitle.find(this->subString) != std::string::npos; };
+            std::function<bool(UzemnaJednotka)> lambdaStartsWith = [&](const auto& uj) { return uj.officialTitle.find(this->subString) == 0; };
+            std::function<bool(UzemnaJednotka)> aktualnaLambda;
+
+            if (retVal == OPT_CONTAINS) {
+                aktualnaLambda = lambdaContains;
+            }
+            if (retVal == OPT_STARTSWITH) {
+                aktualnaLambda = lambdaStartsWith;
+            }
+
+            if (unitType == "kraj") {
+                filtrujZaznamy<Kraj>(this->seqKraje_, aktualnaLambda);
+            }
+            else if (unitType == "okres") {
+                filtrujZaznamy<Okres>(this->seqOkresy_, aktualnaLambda);
+            }
+            else if (unitType == "obec") {
+                filtrujZaznamy<Obec>(this->seqObce_, aktualnaLambda);
+            }
+
+            vypisVystup();
+        }
+    }
+
+    int zistiParametre()
+    {
+        std::cout << "Zadaj substring: ";
+        std::getline(std::cin, subString);
+
+        std::cout << "Zadaj typ [kraj, okres, obec]: ";
+        std::cin >> unitType;
+
+        std::cout << "Zadaj [c]ontains alebo [s]tartsWith: ";
+        std::string userOption;
+        std::cin >> userOption;
+
+        if (userOption.length() > 0) {
+            std::string firstStr = userOption.substr(0, 1);
+            if (firstStr == "c") {
+                return OPT_CONTAINS;
+            }
+            else if (firstStr == "s") {
+                return OPT_STARTSWITH;
+            }
+        }
+
+        std::cout << "Zle zadana moznost" << std::endl;
+        return OPT_INVALID;
+    }
+
+    template<typename DataType, typename LambdaType>
+    void filtrujZaznamy(SequenceType<DataType>* inputSequence, LambdaType filteringLambda)
+    {
+        Algorithm::findElementsWithProperty(
+            inputSequence->begin(),
+            inputSequence->end(),
+            filteringLambda,
+            outputSequence,
+            [](SequenceType<UzemnaJednotka>& result, const DataType& data) {
+                result.insertLast().data_ = data;
+            });
+    }
+
+    void vypisVystup()
+    {
+        std::cout << "Filtrovane zaznamy:" << std::endl << std::endl;
+        size_t pocet = 0;
+        for (const auto& element : outputSequence) {
+            std::cout << element.officialTitle << std::endl;
+            ++pocet;
+        }
+        std::cout << "\nPocet filtrovanych zaznamov: " << pocet << "\n";
+    }
 };
 
